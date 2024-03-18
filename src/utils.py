aa69953ff1dcfd9f2f1283488604018912fe4c6a
Andrew Gaskell <gskll@protonmail.com>
2024-03-18 03:24:34 +0100

    add text2html util and test

---
+++ src/utils.py
@@ -0,0 +1,29 @@
+from src.htmlnode import HTMLNode
+from src.leafnode import LeafNode
+from src.textnode import TextNode, TextNodeType
+
+
+def text_node_to_html_node(text_node: TextNode) -> "HTMLNode":
+    match text_node.text_type:
+        case TextNodeType.TEXT:
+            return LeafNode(value=text_node.text)
+        case TextNodeType.BOLD:
+            return LeafNode(value=text_node.text, tag="b")
+        case TextNodeType.ITALIC:
+            return LeafNode(value=text_node.text, tag="i")
+        case TextNodeType.CODE:
+            return LeafNode(value=text_node.text, tag="code")
+        case TextNodeType.LINK:
+            if not text_node.url:
+                raise ValueError("link text_node with no url")
+            return LeafNode(
+                value=text_node.text, tag="a", props={"href": text_node.url}
+            )
+        case TextNodeType.IMAGE:
+            if not text_node.url:
+                raise ValueError("image text_node with no url")
+            return LeafNode(
+                value="", tag="img", props={"src": text_node.url, "alt": text_node.text}
+            )
+
+    raise Exception("text_node.text_type not recognized")
---
+++ tests/test_utils.py
@@ -0,0 +1,62 @@
+import unittest
+
+from src.leafnode import LeafNode
+from src.textnode import TextNode, TextNodeType
+from src.utils import text_node_to_html_node
+
+
+class TestUtils(unittest.TestCase):
+    def test_text_node_to_html_node_TEXT(self):
+        textnode = TextNode(text="test", text_type=TextNodeType.TEXT)
+        htmlnode = text_node_to_html_node(textnode)
+        self.assertIsInstance(htmlnode, LeafNode)
+        self.assertEqual(htmlnode, LeafNode(value="test"))
+
+    def test_text_node_to_html_node_BOLD(self):
+        textnode = TextNode(text="test", text_type=TextNodeType.BOLD)
+        htmlnode = text_node_to_html_node(textnode)
+        self.assertIsInstance(htmlnode, LeafNode)
+        self.assertEqual(htmlnode, LeafNode(value="test", tag="b"))
+
+    def test_text_node_to_html_node_ITALIC(self):
+        textnode = TextNode(text="test", text_type=TextNodeType.ITALIC)
+        htmlnode = text_node_to_html_node(textnode)
+        self.assertIsInstance(htmlnode, LeafNode)
+        self.assertEqual(htmlnode, LeafNode(value="test", tag="i"))
+
+    def test_text_node_to_html_node_CODE(self):
+        textnode = TextNode(text="test", text_type=TextNodeType.CODE)
+        htmlnode = text_node_to_html_node(textnode)
+        self.assertIsInstance(htmlnode, LeafNode)
+        self.assertEqual(htmlnode, LeafNode(value="test", tag="code"))
+
+    def test_text_node_to_html_node_LINK(self):
+        textnode = TextNode(text="test", text_type=TextNodeType.LINK, url="test_url")
+        htmlnode = text_node_to_html_node(textnode)
+        self.assertIsInstance(htmlnode, LeafNode)
+        self.assertEqual(
+            htmlnode, LeafNode(value="test", tag="a", props={"href": "test_url"})
+        )
+
+    def test_text_node_to_html_node_LINK_no_url(self):
+        textnode = TextNode(text="test", text_type=TextNodeType.LINK)
+        with self.assertRaises(ValueError) as e:
+            text_node_to_html_node(textnode)
+
+        self.assertEqual(str(e.exception), "link text_node with no url")
+
+    def test_text_node_to_html_node_IMAGE(self):
+        textnode = TextNode(text="test", text_type=TextNodeType.IMAGE, url="test_url")
+        htmlnode = text_node_to_html_node(textnode)
+        self.assertIsInstance(htmlnode, LeafNode)
+        self.assertEqual(
+            htmlnode,
+            LeafNode(value="", tag="img", props={"src": "test_url", "alt": "test"}),
+        )
+
+    def test_text_node_to_html_node_IMAGE_no_url(self):
+        textnode = TextNode(text="test", text_type=TextNodeType.IMAGE)
+        with self.assertRaises(ValueError) as e:
+            text_node_to_html_node(textnode)
+
+        self.assertEqual(str(e.exception), "image text_node with no url")
