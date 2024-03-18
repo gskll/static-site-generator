3779b00bdfbca03c0c32a09712c170a5f8cca883
Andrew Gaskell <gskll@protonmail.com>
2024-03-18 03:30:14 +0100

    add htmlNode eq

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -29,3 +29,14 @@ class HTMLNode:
 
     def __repr__(self) -> str:
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, HTMLNode):
+            return False
+
+        return (
+            self.tag != other.tag
+            and self.value != other.value
+            and self.children != other.children
+            and self.props != other.props
+        )
