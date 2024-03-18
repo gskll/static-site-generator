07abcac10ed822d7f09caa7cce63bc9b109431a6
Andrew Gaskell <gskll@protonmail.com>
2024-03-18 02:42:02 +0100

    update textnode tests with enum

--- tests/test_textnode.py
+++ tests/test_textnode.py
@@ -1,31 +1,33 @@
 import unittest
 
-from textnode import TextNode
+from src.textnode import TextNode, TextNodeType
 
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = TextNode("This is a text node", "bold")
-        node2 = TextNode("This is a text node", "bold")
+        node = TextNode("This is a text node", TextNodeType.BOLD)
+        node2 = TextNode("This is a text node", TextNodeType.BOLD)
         self.assertEqual(node, node2)
 
     def test_diff_text(self):
-        node = TextNode("This is a text node", "bold")
-        node2 = TextNode("This is a different text node", "bold")
+        node = TextNode("This is a text node", TextNodeType.BOLD)
+        node2 = TextNode("This is a different text node", TextNodeType.BOLD)
         self.assertNotEqual(node, node2)
 
     def test_diff_text_type(self):
-        node = TextNode("This is a text node", "bold")
-        node2 = TextNode("This is a text node", "normal")
+        node = TextNode("This is a text node", TextNodeType.BOLD)
+        node2 = TextNode("This is a text node", TextNodeType.TEXT)
         self.assertNotEqual(node, node2)
 
     def test_diff_url(self):
-        node = TextNode("This is a text node", "bold", "someUrl.com")
-        node2 = TextNode("This is a different text node", "bold", "anotherUrl.com")
+        node = TextNode("This is a text node", TextNodeType.BOLD, "someUrl.com")
+        node2 = TextNode(
+            "This is a different text node", TextNodeType.BOLD, "anotherUrl.com"
+        )
         self.assertNotEqual(node, node2)
 
     def test_default_url(self):
-        node = TextNode("This is a text node", "bold")
+        node = TextNode("This is a text node", TextNodeType.BOLD)
         self.assertIsNone(node.url)
 
 
