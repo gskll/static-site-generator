17afb724d3176a59a4a0e021a30f4d1e455f3723
Andrew Gaskell <gskll@protonmail.com>
2024-03-18 05:26:41 +0100

    fix extract markdown function signatures

--- src/utils/extract_markdown.py
+++ src/utils/extract_markdown.py
@@ -1,11 +1,11 @@
 import re
 
 
-def extract_markdown_images(text: str) -> list[tuple[str]]:
+def extract_markdown_images(text: str) -> list[tuple[str, str]]:
     image_re = r"!\[(.*?)\]\((.*?)\)"
     return re.findall(image_re, text)
 
 
-def extract_markdown_links(text: str) -> list[tuple[str]]:
+def extract_markdown_links(text: str) -> list[tuple[str, str]]:
     link_re = r"\[(.*?)\]\((.*?)\)"
     return re.findall(link_re, text)
