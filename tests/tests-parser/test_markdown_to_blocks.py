6266ac9f6e25742909d48a112a61874594767dfd
Andrew Gaskell <gskll@protonmail.com>
2024-03-19 05:44:16 +0100

    naming

---
+++ tests/tests-parser/test_markdown_to_blocks.py
@@ -0,0 +1,30 @@
+import unittest
+
+from src.parser.split_markdown_blocks import split_markdown_blocks
+
+
+class TestMarkdownToBlocks(unittest.TestCase):
+    def test_works_with_one_block(self):
+        text = "This is a\nmulti-line\nparagraph"
+        blocks = split_markdown_blocks(text)
+        want = ["This is a\nmulti-line\nparagraph"]
+        self.assertEqual(want, blocks)
+
+    def test_works_with_multiple_blocks(self):
+        text = "This is **bolded** paragraph\n\nThis is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line\n\n* This is a list\n* with items"
+        blocks = split_markdown_blocks(text)
+        want = [
+            "This is **bolded** paragraph",
+            "This is another paragraph with *italic* text and `code` here\nThis is the same paragraph on a new line",
+            "* This is a list\n* with items",
+        ]
+        self.assertEqual(want, blocks)
+
+    def test_works_with_indentation(self):
+        text = "* list item 1\n* list item 2\n\n    * indented list item1\n   *indented list item2"
+        blocks = split_markdown_blocks(text)
+        want = [
+            "* list item 1\n* list item 2",
+            "    * indented list item1\n   *indented list item2",
+        ]
+        self.assertEqual(want, blocks)
