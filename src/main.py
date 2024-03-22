3deda2b5e738077abe82590112c3476a2428d073
Andrew Gaskell <gskll@protonmail.com>
2024-03-22 04:37:03 +0100

    update main.py to use recursive

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
-from src.renderer.generate_page import generate_page
+from src.renderer.generate_page_recursive import generate_page_recursive
 from src.utils import tree_copy
 
 
 def main():
     tree_copy("./static", "./public")
-    generate_page("./content/index.md", "./template.html", "./public/index.html")
+    generate_page_recursive("./content", "./template.html", "./public")
 
 
 main()
