6a84b39257cc0d0b13766210786c1790f30a5924
Andrew Gaskell <gskll@protonmail.com>
2024-03-22 02:38:58 +0100

    refactor generate page

--- src/renderer/generate_page.py
+++ src/renderer/generate_page.py
@@ -5,49 +5,29 @@ from .markdown_to_html_node import markdown_to_html_node
 
 
 def generate_page(from_path: str, template_path: str, dest_path: str):
-    empty_path_args = []
-    if from_path == "":
-        empty_path_args.append("from_path")
-    if template_path == "":
-        empty_path_args.append("template_path")
-    if dest_path == "":
-        empty_path_args.append("dest_path")
-
-    if len(empty_path_args) > 0:
-        empty_paths = ", ".join(empty_path_args)
-        raise ValueError(f"path args cannot be empty: {empty_paths}")
+    validate_paths(from_path, template_path, dest_path)
 
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
-    if not exists(from_path) or not isfile(from_path):
-        raise FileNotFoundError(f"from_path must be a valid file path: {from_path}")
+    md_doc = read_file(from_path)
+    templ_doc = read_file(template_path)
 
-    with open(from_path, "r") as md_file:
-        md_doc = md_file.read()
-        if not md_doc:
-            raise ValueError(f"from_path file {from_path} is empty")
-
-    if not exists(template_path) or not isfile(template_path):
-        raise FileNotFoundError(
-            f"template_path must be a valid file path: {template_path}"
-        )
-
-    with open(template_path, "r") as tmpl_file:
-        tmpl_doc = tmpl_file.read()
-        if not tmpl_doc:
-            raise ValueError(f"template_path file {template_path} is empty")
-
-    content = markdown_to_html_node(md_doc).to_html()
+    try:
+        html_content = markdown_to_html_node(md_doc).to_html()
+    except Exception as e:
+        raise Exception(f"Error converting markdown to html: {e}")
 
     try:
         title = extract_page_title(md_doc)
     except Exception as e:
         raise Exception(f"Error extracting page title: {e}")
 
-    page = tmpl_doc.replace("{{ Title }}", title).replace("{{ Content }}", content)
+    page = templ_doc.replace("{{ Title }}", title).replace(
+        "{{ Content }}", html_content
+    )
 
     dest_dir = dirname(dest_path)
-    if not exists(dest_dir) or not isdir(dest_dir):
+    if not isdir(dest_dir):
         try:
             makedirs(dest_dir)
         except Exception as e:
@@ -57,3 +37,32 @@ def generate_page(from_path: str, template_path: str, dest_path: str):
 
     with open(dest_path, "w") as html_file:
         html_file.write(page)
+
+
+def read_file(path: str) -> str:
+    try:
+        with open(path, "r") as f:
+            content = f.read()
+    except IOError as e:
+        raise IOError(f"Error reading file {path}: {e}")
+
+    if not content:
+        raise ValueError(f"The file {path} is empty (0 bytes)")
+
+    return content
+
+
+def validate_paths(from_path: str, template_path: str, dest_path: str) -> None:
+    empty_paths = [p for p in (from_path, template_path, dest_path) if not p]
+    if empty_paths:
+        raise ValueError(
+            f"the following path arguments cannot be empty: {', '.join(empty_paths)}"
+        )
+
+    if not isfile(from_path):
+        raise FileNotFoundError(f"from_path must be a valid file path: {from_path}")
+
+    if not isfile(template_path):
+        raise FileNotFoundError(
+            f"template_path must be a valid file path: {template_path}"
+        )
